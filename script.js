338d293bf753194d51aa8c2d2e5433456b617f8f
Emanuil <enikolov.intl@gmail.com>
2018-08-01 18:11:58 +0300

    Update Check

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let randomBtn = document.getElementById("random");
 function setRandomColor() {
     let letters = '0123456789ABCDEF';
     let color = '#';
+    
     for (let i = 0; i < 6; ++i) {
         color += letters[Math.floor(Math.random() * 16)];
     }
